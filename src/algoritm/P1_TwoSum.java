035c8b004f4aa4425d6c187c3d2c42e44c7aac86
krn-singh <karan_kalewal@yahoo.co.in>
2019-01-09 21:54:01 -0500

    Two Sum Complete

--- src/algoritm/P1_TwoSum.java
+++ src/algoritm/P1_TwoSum.java
@@ -1,5 +1,8 @@
 package algoritm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
@@ -19,6 +22,19 @@ return [0, 1].
 
 public class P1_TwoSum {
 
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment)) {
+                return new int[]{map.get(compliment),i};
+            }               
+            
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
